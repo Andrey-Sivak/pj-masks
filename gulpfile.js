7097f6233e76ecc91a4946b9e4a88e4f5ca6b929
a.sivak <riziy_bes@mail.ru>
2022-02-02 20:34:42 +0300

    next

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
+const sass = require('gulp-sass')(require('sass'));
 const del = require('del');
 const browserSync = require('browser-sync').create();
-const imagemin = require('gulp-imagemin');
 const webpack = require('webpack-stream');
-const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const replace = require('gulp-replace');
 
@@ -38,10 +34,8 @@ function fonts() {
 function styles() {
 
 	return gulp.src('./app/sass/**/*.scss')
-		// .pipe(sourcemaps.init())
-		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(postcss([ autoprefixer() ]))
-		// .pipe(sourcemaps.write('.'))
 		.pipe(replace('../../', '../'))
 		.pipe(gulp.dest('./dist/css'))
 		.pipe(browserSync.stream());
@@ -56,11 +50,6 @@ function scripts() {
 
 function img() {
 	return gulp.src('./app/img/**/**/**/*')
-		/*.pipe(imagemin([
-	    imagemin.gifsicle({interlaced: true}),
-	    imagemin.mozjpeg({progressive: true}),
-	    imagemin.optipng({optimizationLevel: 5})
-		]))*/
 		.pipe(gulp.dest('./dist/img'))
 }
 
@@ -95,7 +84,7 @@ gulp.task('watch', watch);
 gulp.task('img', img);
 
 let build = gulp.series(clean,
-	gulp.parallel(gulp.series( styles ), scripts, /*php,*/ img, svg, html, fonts)
+	gulp.parallel(gulp.series( styles ), scripts, img, svg, html, fonts)
 );
 
 gulp.task('build', build);
